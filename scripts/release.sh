2269883212fa6a57de87abf2c7e5e83a287744d3
Michael Mandato <mmandato@helloencom.co>
2017-06-25 16:22:47 -0400

    Update fastcgi_pass server name and make bootstrap/cache dir for laravel writable

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,7 @@ tar -xf /var/deploy/archive.tar.gz -C /var/www/laravel --strip 1
 cd /var/www/laravel/public
 
 chmod -R 777 /var/www/laravel/storage
+chmod -R 777 /var/www/laravel/bootstrap/cache
 
 pwd
 
